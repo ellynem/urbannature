0045fc3aa01f92d6b80fc5b721ddf1adfef9507c
ellynem <maryellenellefsen@gmail.com>
2023-12-17 18:40:48 +0100

    Fehlerbehebung commit

---
+++ Assets/Scripts/DatabaseDuck.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class DatabaseDuck
+{
+    public string messages;
+    public string url;
+}
