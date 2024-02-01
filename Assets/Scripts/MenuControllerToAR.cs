957a372afa2e8de10fa470c0dd4d5836d1409ec3
ellynem <maryellenellefsen@gmail.com>
2024-02-01 10:02:05 +0100

    Merge conflict fix

--- Assets/Scripts/MenuControllerToAR.cs
+++ Assets/Scripts/MenuControllerToAR.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,22 +15,4 @@ public class MenuController : MonoBehaviour
         SceneManager.LoadScene(AR);
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MenuController : MonoBehaviour
-{
-    // Methode zum Laden der AR-Szene mit dem entsprechenden Objekt
-    public void LoadARScene(string AR)
-    {
-        // Hier können Sie Informationen über das gewählte Objekt speichern oder übergeben
-        // ...
-
-        // Laden der AR-Szene
-        SceneManager.LoadScene(AR);
-    }
-}
->>>>>>> 1e93e260f93a896bcfe8329f263b8666a2f707dd
